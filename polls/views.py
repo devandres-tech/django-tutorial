1e4f2b3d8489595670ad306e41d4ae378189bcd9
andres alcocer <devandres.tech@gmail.com>
2022-06-20 19:10:24 -0400

    configured routing

--- polls/views.py
+++ polls/views.py
@@ -1,1 +1,9 @@
+from django.http import HttpResponse
 
+
+def index(request):
+	return HttpResponse('Hello, world. You are at that polls index.')
+
+
+def hello(request):
+	return HttpResponse('got some json?')
